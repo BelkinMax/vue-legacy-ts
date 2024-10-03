aa1cf34a1cce78e1f97eda387ce2e746183c2cac
Maxim Belkin <maximbe@ext.bershka.com>
2024-10-03 10:36:19 +0200

    feat: example component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,16 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
-  ]
+  'root': true,
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+  ],
+  rules: {
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'object-curly-spacing': ['error', 'always'],
+  },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
-<script setup lang="ts">
-const msg = 'Hello Vue 3 + TypeScript!'
+<script setup>
+import { ref } from 'vue';
+import TheButton from '@/components/TheButton.vue';
+
+const counter = ref(0)
+
+function onClick() {
+  counter.value++
+}
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <h1 class="text-3xl font-bold underline text-amber-500">
-        {{ msg }}
-      </h1>
-    </header>
+  <div id="app" class="w-full min-h-screen p-4 flex justify-center items-center">
+    <TheButton :action="onClick">
+      Button clicked {{ counter }} times
+    </TheButton>
   </div>
 </template>
